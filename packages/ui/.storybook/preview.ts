faef9b72f1f83503069e22130a6a38da3d2e99e6
Sanjar Mirakhmedov <sanjarmirakhmedov@Sanjars-MacBook-Pro.local>
2025-07-28 11:12:25 +0500

    feat(mui): [STMS-4635] create basic theme

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,13 +1,9 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
-import { lightTheme, darkTheme } from "../src/theme";
+import { ThemeProvider } from "../src/theme";
 
 export const decorators = [
   withThemeFromJSXProvider({
-    themes: {
-      light: lightTheme,
-      dark: darkTheme,
-    },
     defaultTheme: "light",
     Provider: ThemeProvider,
     GlobalStyles: CssBaseline,
---
+++ packages/ui/src/theme.tsx
@@ -0,0 +1,24 @@
+import {
+  createTheme as createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material/styles";
+import type { ReactNode } from "react";
+
+function createTheme() {
+  const theme = createMuiTheme({
+    colorSchemes: {
+      dark: true,
+      light: true,
+    },
+    cssVariables: {
+      colorSchemeSelector: ".sd-theme-%s",
+    },
+  });
+
+  return theme;
+}
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const theme = createTheme();
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+}
