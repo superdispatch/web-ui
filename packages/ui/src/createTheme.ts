8f9e7f9fcd94754c23ec39f9057473a671b83181
Sanjar Mirakhmedov <sanjarmirakhmedov@Sanjars-MacBook-Pro.local>
2025-07-28 16:57:42 +0500

    feat(mui): [STMS-4635] separate createTheme into its own file

---
+++ packages/ui/src/createTheme.ts
@@ -0,0 +1,54 @@
+import { type ColorSystemOptions } from "@mui/material";
+import { createTheme as createMuiTheme } from "@mui/material/styles";
+import { Color, ColorDark } from "./color";
+
+export function createTheme() {
+  const theme = createMuiTheme({
+    colorSchemes: {
+      light: {
+        palette: createPalette("light"),
+      },
+      dark: {
+        palette: createPalette("dark"),
+      },
+    },
+    cssVariables: {
+      colorSchemeSelector: ".sd-theme-%s",
+    },
+  });
+
+  return theme;
+}
+
+function createPalette(
+  colorScheme: "light" | "dark"
+): ColorSystemOptions["palette"] {
+  const color = colorScheme === "light" ? Color : ColorDark;
+
+  return {
+    primary: {
+      main: color.Blue300,
+    },
+    error: {
+      main: color.Red300,
+    },
+    action: {
+      hover: color.Silver200,
+      selected: color.Blue50,
+      disabled: color.Silver400,
+    },
+    text: {
+      primary: color.Dark500,
+      secondary: color.Dark300,
+      disabled: color.Dark100,
+    },
+    common: {
+      white: color.White,
+      black: color.Black,
+    },
+    divider: color.Silver400,
+    background: {
+      paper: color.White,
+    },
+  };
+}
--- packages/ui/src/theme.tsx
+++ packages/ui/src/theme.tsx
@@ -1,15 +1,8 @@
-import {
-  CssBaseline,
-  GlobalStyles,
-  type ColorSystemOptions,
-} from "@mui/material";
-import {
-  createTheme as createMuiTheme,
-  ThemeProvider as MuiThemeProvider,
-} from "@mui/material/styles";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import type { ReactNode } from "react";
-import { Color, ColorDark } from "./color";
 import { createCssColorVariables } from "./createCssColorVariables";
+import { createTheme } from "./createTheme";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const theme = createTheme();
@@ -26,57 +19,6 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
       </CssBaseline>
     </MuiThemeProvider>
   );
-}
-
-function createTheme() {
-  const theme = createMuiTheme({
-    colorSchemes: {
-      light: {
-        palette: createPalette("light"),
-      },
-      dark: {
-        palette: createPalette("dark"),
-      },
-    },
-    cssVariables: {
-      colorSchemeSelector: ".sd-theme-%s",
-    },
-  });
-
-  return theme;
-}
-
-function createPalette(
-  colorScheme: "light" | "dark"
-): ColorSystemOptions["palette"] {
-  const color = colorScheme === "light" ? Color : ColorDark;
-
-  return {
-    primary: {
-      main: color.Blue300,
-    },
-    error: {
-      main: color.Red300,
-    },
-    action: {
-      hover: color.Silver200,
-      selected: color.Blue50,
-      disabled: color.Silver400,
-    },
-    text: {
-      primary: color.Dark500,
-      secondary: color.Dark300,
-      disabled: color.Dark100,
-    },
-    common: {
-      white: color.White,
-      black: color.Black,
-    },
-    divider: color.Silver400,
-    background: {
-      paper: color.White,
-    },
-  };
 }
 
 export function ThemeCssVariables() {
