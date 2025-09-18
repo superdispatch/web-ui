3d9dcd801969541c8789fbdbb9426af69b8588e7
SiriusCrain <yuriy.matskanyuk@gmail.com>
2025-09-18 17:37:57 +0500

    fix: Typography build

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./Button";
+export * from "./Typography";
 export { ThemeProvider } from "./theme";
 export { Color, ColorDark, ColorDynamic } from "./color";
 export { useColorScheme } from "./useColorScheme";
