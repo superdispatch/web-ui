47e4eadfba08c56f0962bae4d2e14aa7895f6dc2
Yuriy Matskanyuk <yuriy.matskanyuk@gmail.com>
2025-09-23 13:46:50 +0500

    chore: [STMS-4898] Add POC of Button component

--- packages/ui/src/Button/Button.stories.tsx
+++ packages/ui/src/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./Button";
 
-import type { Meta, StoryObj } from "@storybook/react-vite";
+import type { Args, Meta, StoryObj } from "@storybook/react-vite";
+import type { ButtonProps } from "./Button";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
@@ -15,5 +16,27 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     children: "Button",
-  },
+  } as ButtonProps,
+  render: (args: Args) => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+      <Button {...args} variant="primary">
+        Primary
+      </Button>
+      <Button {...args} variant="secondary">
+        Secondary
+      </Button>
+      <Button {...args} variant="inverted">
+        Inverted
+      </Button>
+      <Button {...args} variant="text">
+        Text
+      </Button>
+      <Button {...args} variant="critical">
+        Critical
+      </Button>
+      <Button {...args} variant="success">
+        Success
+      </Button>
+    </div>
+  ),
 };
--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,9 +1,62 @@
-import MuiButton from "@mui/material/Button";
+import ButtonBase from "@mui/material/ButtonBase";
+import React from "react";
+import { ColorDynamic } from "../color";
+import type { ButtonVariant } from "./constants";
 
-export type ButtonProps = {
-  children: React.ReactNode;
+const commonButtonStyle = {
+  padding: "6px 16px",
+  borderRadius: "4px",
+  minWidth: "32px",
+  fontSize: "14px",
+  fontWeight: 600,
+  textTransform: "none",
+  fontFamily: "Inter",
+  fontStyle: "normal",
+  lineHeight: "22px",
 };
 
-export function Button({ children }: ButtonProps) {
-  return <MuiButton variant="contained">{children}</MuiButton>;
+const buttonStyleMap: Record<ButtonVariant, React.CSSProperties | object> = {
+  critical: {
+    ...commonButtonStyle,
+  },
+  text: {
+    ...commonButtonStyle,
+  },
+  success: {
+    ...commonButtonStyle,
+  },
+  inverted: {
+    ...commonButtonStyle,
+  },
+  secondary: {
+    ...commonButtonStyle,
+  },
+  primary: {
+    ...commonButtonStyle,
+    backgroundColor: ColorDynamic.Blue300,
+    color: "#fff",
+    "&:hover": {
+      opacity: 0.7,
+    },
+  },
+};
+
+function getButtonStyle(variant?: ButtonVariant) {
+  return buttonStyleMap[variant ?? "primary"];
 }
+
+export interface ButtonProps {
+  children?: React.ReactNode;
+  variant?: ButtonVariant;
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  const { variant, ...rest } = props;
+  const buttonStyle = getButtonStyle(variant);
+
+  return (
+    <ButtonBase {...rest} sx={{ ...buttonStyle }}>
+      {props.children}
+    </ButtonBase>
+  );
+};
