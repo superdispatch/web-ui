19c08c25e323d039134403612393cae5e5077cad
Sanjar Mirakhmedov <sanjarmirakhmedov@Sanjars-MacBook-Pro.local>
2025-09-29 16:11:54 +0500

    feat(tag): [STMS-4933] implement tag component

---
+++ packages/ui/src/Tag/Tag.stories.tsx
@@ -0,0 +1,100 @@
+import { Box } from "@mui/material";
+import { Tag, type TagProps } from "./Tag";
+import { TAG_COLORS, TAG_VARIANTS } from "./constants";
+import type { StoryFn, Meta } from "@storybook/react-vite";
+import { CheckmarkIcon } from "../internal/icons";
+
+type Args = Omit<TagProps, "startIcon" | "endIcon"> & {
+  startIcon?: boolean;
+  endIcon?: boolean;
+};
+
+const meta: Meta<Args> = {
+  title: "Components/Tag",
+  component: Tag,
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+      },
+      options: TAG_COLORS,
+    },
+    variant: {
+      control: {
+        type: "select",
+      },
+      options: TAG_VARIANTS,
+    },
+    isBold: {
+      control: {
+        type: "boolean",
+      },
+    },
+    startIcon: {
+      control: {
+        type: "boolean",
+      },
+    },
+    endIcon: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+function TagRenderer(args: Args) {
+  return (
+    <Tag
+      {...args}
+      startIcon={args.startIcon ? <CheckmarkIcon /> : undefined}
+      endIcon={args.endIcon ? <CheckmarkIcon /> : undefined}
+    />
+  );
+}
+
+const Template: StoryFn<Args> = (args) => <TagRenderer {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: "Text",
+  startIcon: false,
+  endIcon: false,
+};
+
+const AllVariantsAndColorsTemplate: StoryFn<Omit<Args, "variant" | "color">> = (
+  args
+) => (
+  <>
+    {TAG_VARIANTS.map((variant) => (
+      <Box
+        key={variant}
+        sx={{ display: "flex", alignItems: "center", gap: 2, mb: 2 }}
+      >
+        {TAG_COLORS.map((color) => (
+          <Box key={color} sx={{ display: "flex", justifyContent: "center" }}>
+            <TagRenderer {...args} color={color} variant={variant} />
+          </Box>
+        ))}
+      </Box>
+    ))}
+  </>
+);
+
+export const AllVariantsAndColors = AllVariantsAndColorsTemplate.bind({});
+
+AllVariantsAndColors.args = {
+  label: "Text",
+  startIcon: false,
+  endIcon: false,
+};
+
+AllVariantsAndColors.parameters = {
+  controls: {
+    exclude: ["variant", "color"],
+  },
+};
---
+++ packages/ui/src/Tag/Tag.tsx
@@ -0,0 +1,138 @@
+import { Box } from "@mui/material";
+import { ColorDynamic } from "../color";
+import { Typography } from "../Typography";
+import type { TAG_COLORS, TAG_VARIANTS } from "./constants";
+
+export type TagColor = (typeof TAG_COLORS)[number];
+export type TagVariant = (typeof TAG_VARIANTS)[number];
+
+export interface TagProps {
+  label: string;
+  color?: TagColor;
+  variant?: TagVariant;
+  isBold?: boolean;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+}
+
+const variantToColorMap: Record<
+  TagVariant,
+  Record<
+    TagColor,
+    {
+      color: string;
+      backgroundColor: string;
+    }
+  >
+> = {
+  filled: {
+    blue: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Blue300,
+    },
+    teal: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Teal300,
+    },
+    gray: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Dark300,
+    },
+    green: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Green300,
+    },
+    purple: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Purple300,
+    },
+    red: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Red300,
+    },
+    yellow: {
+      color: ColorDynamic.White,
+      backgroundColor: ColorDynamic.Yellow300,
+    },
+  },
+  subtle: {
+    blue: {
+      color: ColorDynamic.Blue500,
+      backgroundColor: ColorDynamic.Blue50,
+    },
+    teal: {
+      color: ColorDynamic.Teal500,
+      backgroundColor: ColorDynamic.Teal50,
+    },
+    gray: {
+      color: ColorDynamic.Dark300,
+      backgroundColor: ColorDynamic.Silver200,
+    },
+    green: {
+      color: ColorDynamic.Green500,
+      backgroundColor: ColorDynamic.Green50,
+    },
+    purple: {
+      color: ColorDynamic.Purple500,
+      backgroundColor: ColorDynamic.Purple50,
+    },
+    red: {
+      color: ColorDynamic.Red500,
+      backgroundColor: ColorDynamic.Red50,
+    },
+    yellow: {
+      color: ColorDynamic.Yellow500,
+      backgroundColor: ColorDynamic.Yellow50,
+    },
+  },
+};
+
+export function Tag({
+  label,
+  variant = "filled",
+  color = "blue",
+  isBold = true,
+  startIcon,
+  endIcon,
+}: TagProps) {
+  const colorMap = variantToColorMap[variant][color];
+
+  return (
+    <Box
+      sx={(theme) => ({
+        color: colorMap.color,
+        backgroundColor: colorMap.backgroundColor,
+        borderRadius: "4px",
+        height: "22px",
+        paddingLeft: theme.spacing(0.5),
+        paddingRight: theme.spacing(0.5),
+        display: "inline-flex",
+        alignItems: "center",
+        gap: theme.spacing(0.5),
+
+        [theme.breakpoints.only("xs")]: {
+          height: "26px",
+        },
+      })}
+    >
+      {startIcon && <TagIcon>{startIcon}</TagIcon>}
+
+      <Typography variant={isBold ? "body-semibold" : "body"}>
+        {label}
+      </Typography>
+
+      {endIcon && <TagIcon>{endIcon}</TagIcon>}
+    </Box>
+  );
+}
+
+function TagIcon({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      component="span"
+      sx={{ display: "inherit", "& > :nth-of-type(1)": { fontSize: "16px" } }}
+    >
+      {children}
+    </Box>
+  );
+}
